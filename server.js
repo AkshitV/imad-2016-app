b4a47615d9a28699e8ce68a5eda75ab9aaea32c1
AkshitV <akshitvaid@gmail.com>
2016-11-08 23:37:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/photo.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'photo.jpg'));
 });
 
 
